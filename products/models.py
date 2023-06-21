d876ecb438683cfccc9b1f543191114c97247706
Unnati Pradhan <pradhankhushi411@gmail.com>
2023-06-21 23:45:19 +0530

    updated

--- products/models.py
+++ products/models.py
@@ -6,8 +6,8 @@ class ProductCategoryModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    # def __str__(self):
-        # return self.title
+    def __str__(self):
+        return self.title
 
 class ProductBrandModel(models.Model):
     title = models.CharField(max_length=100, null=True, blank=True, unique=True)
